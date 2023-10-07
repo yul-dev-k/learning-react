c7f493e3e3bccf65acf6749b635a0cbe85862362
kimgyul <142677224+yul-dev-k@users.noreply.github.com>
2023-10-07 14:49:43 +0900

    footer 내용 수정

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -1,7 +1,25 @@
+import { Link } from "react-router-dom";
+import { FaYoutube, FaTwitter } from "react-icons/fa6";
+import "./Footer.scss";
+
 export default function Footer() {
   return (
     <footer>
-      <h1>Footer</h1>
+      <h1>Jiyul</h1>
+      <p>2023 KimJiyul &copy; All Rights Reserved.</p>
+
+      <ul>
+        <li>
+          <Link to="/">
+            <FaYoutube size={20} />
+          </Link>
+        </li>
+        <li>
+          <Link to="/">
+            <FaTwitter size={20} />
+          </Link>
+        </li>
+      </ul>
     </footer>
   );
 }
