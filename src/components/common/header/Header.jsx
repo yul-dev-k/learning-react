e325da84b91f16d3c7e8a1aff83302503872c5e2
kimgyul <142677224+yul-dev-k@users.noreply.github.com>
2023-10-07 13:44:39 +0900

    NavLink로 메뉴 활성화 처리. NavLink는 activeClassName 속성이 있어 활성화가 되면 on 클래스가 생김

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,15 +1,39 @@
 import "./Header.scss";
+import { NavLink, Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <header>
-      <h1>React</h1>
+      <h1>
+        <Link to="/">React</Link>
+      </h1>
+
       <ul>
-        <li>Department</li>
-        <li>Galley</li>
-        <li>Youtube</li>
-        <li>Members</li>
-        <li>Contact</li>
+        <li>
+          <NavLink to="/department" activeClassName={"on"}>
+            Department
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/gallery" activeClassName={"on"}>
+            Gallery
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/youtube" activeClassName={"on"}>
+            Youtube
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/members" activeClassName={"on"}>
+            Members
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact" activeClassName={"on"}>
+            Contact
+          </NavLink>
+        </li>
       </ul>
     </header>
   );
