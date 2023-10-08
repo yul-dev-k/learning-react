3a0931975bc65f8a855f0615def417cbe861b7bd
kimgyul <142677224+yul-dev-k@users.noreply.github.com>
2023-10-08 15:04:33 +0900

    서브페이지 텍스트 모션 처리

--- src/components/common/layout/Layout.jsx
+++ src/components/common/layout/Layout.jsx
@@ -3,13 +3,16 @@ import "./Layout.scss";
 
 export default function Layout({ children, title }) {
   let newClass = title.toLowerCase().split(" ").join("_");
+  const refFrame = useRef(null);
   const refTitle = useRef(null);
 
   useEffect(() => {
     // 가상돔을 제어해야하면 무조건 useEffect쓴다고 생각하면 됨.
+
     let text = refTitle.current.innerText;
     let tags = "";
     let count = 0;
+
     for (let letter of text) {
       count++;
       tags += `<span style='display:inline-block; transition-delay:${
@@ -17,10 +20,15 @@ export default function Layout({ children, title }) {
       }s'>${letter}</span>`;
     }
     refTitle.current.innerHTML = tags;
+
+    setTimeout(() => {
+      refFrame.current.classList.add("on");
+    }, 300);
   }, []);
   return (
-    <section className={`layout ${newClass}`}>
+    <section ref={refFrame} className={`layout ${newClass}`}>
       <h1 ref={refTitle}>{title}</h1>
+      <div className="bar"></div>
       {children}
     </section>
   );
