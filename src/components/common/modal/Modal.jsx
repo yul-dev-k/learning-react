e3681e69588c71e67a8f21c4117040f0e29d1a57
kimgyul <142677224+yul-dev-k@users.noreply.github.com>
2023-10-08 13:39:21 +0900

    &&연산자를 통한 모달 컴포넌트 마운트

---
+++ src/components/common/modal/Modal.jsx
@@ -0,0 +1,5 @@
+import "./Modal.scss";
+
+export default function Modal() {
+  return <aside className="modal">Modal</aside>;
+}
--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -1,29 +1,22 @@
-import { useRef } from "react";
+import { useState } from "react";
 import Layout from "../../common/layout/Layout";
 import "./Department.scss";
+import Modal from "../../common/modal/Modal";
 
 export default function Department() {
-  console.log("re-render");
-  const rotate = useRef(0);
-
-  // 리액트에서 document.querySelector로 돔 요소를 선택하면 안되는 이유
-  const box = useRef(null); // 처음엔 null -> 이후엔 {current: article} 담김.
-
-  const plus = () => {
-    ++rotate.current;
-    box.current.style.transform = `rotate(${45 * rotate.current}deg)`; // 초기 렌더 이후 re-render 콘솔이 뜨지 않음.
-  };
-  const minus = () => {
-    --rotate.current;
-    box.current.style.transform = `rotate(${45 * rotate.current}deg)`; // 초기 렌더 이후 re-render 콘솔이 뜨지 않음.
-  };
-
+  const [Open, setOpen] = useState(false);
   return (
     <Layout title={"Department"}>
-      <button onClick={minus}>left</button>
-      <button onClick={plus}>right</button>
-
-      <article ref={box}></article>
+      <button onClick={() => setOpen(true)}>open</button>
+      {Open && <Modal />}
     </Layout>
   );
 }
+
+/* 
+  return믄 바깥에는 모든 스크립트 구문을 활용 가능
+  단, JSX 구문 안쪽에서는 {}를 통해서 할 수 있는 연산 3가지
+  1. 변수치환
+  2. map으로 반복 처리
+  3. 삼항연산자 혹은 &&를 통한 분기 처리
+*/
