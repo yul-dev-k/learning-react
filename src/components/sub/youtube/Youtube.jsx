b9c3b01077856b66458ae63e942f3a5ab2dfc80b
kimgyul <142677224+yul-dev-k@users.noreply.github.com>
2023-10-14 15:13:19 +0900

    유튜브 목록, 썸네일 출력

--- src/components/sub/youtube/Youtube.jsx
+++ src/components/sub/youtube/Youtube.jsx
@@ -1,9 +1,38 @@
+import { useEffect, useState } from "react";
 import Layout from "../../common/layout/Layout";
+import { useFetch } from "../../../hooks/useFetch";
 
 export default function Youtube() {
+  const [Vids, setVids] = useState([]);
+  const fetchData = useFetch();
+  console.log(Vids);
+
+  useEffect(() => {
+    const api_key = "AIzaSyAOEJx8xulXWN-r1P2H4abEsM18Vc8r1EI";
+    const pid = "PL0ggp_5dyucDupa2ANtMUuDuURatz4Y70";
+    const num = 10;
+    const baseUrl = `https://www.googleapis.com/youtube/v3/playlistItems?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
+    // fetchData(baseUrl, setVids);
+    fetch(baseUrl)
+      .then((data) => data.json())
+      .then((json) => setVids(json.items));
+  }, []);
   return (
     <Layout title={"Youtube"}>
-      <p>Youtube</p>
+      {Vids.map((data, idx) => {
+        return (
+          <article key={idx}>
+            <h2>{data.snippet.title}</h2>
+            <p>{data.snippet.description}</p>
+            <div className="pic">
+              <img
+                src={data.snippet.thumbnails.standard.url}
+                alt={data.snippet.title}
+              />
+            </div>
+          </article>
+        );
+      })}
     </Layout>
   );
 }
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -3,7 +3,9 @@ export function useFetch() {
     fetch(url)
       .then((data) => data.json())
       .then((json) => {
+        const arr = Object.values(json).filter((data) => data.length);
+        const result = arr.filter((data) => data.length);
         setKey && setKey(Object.keys(json)[0]);
-        setValue(Object.values(json)[0]);
+        setValue(result);
       });
 }
