37483f572b3542499c8bae7f78a029d76416ee8e
kimgyul <142677224+yul-dev-k@users.noreply.github.com>
2023-10-21 13:17:15 +0900

    환경병수 파일에 api키 등록

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -4,12 +4,11 @@ import { useState, useEffect } from "react";
 
 export default function Gallery() {
   const [Pics, setPics] = useState([]);
-  console.log(Pics);
 
   const fetchFlicker = async () => {
     const baseURL =
       "https://www.flickr.com/services/rest/?format=json&nojsoncallback=1";
-    const key = "2a1a0aebb34012a99c23e13b49175343";
+    const key = process.env.REACT_APP_FLICKER_KEY;
     const method_interest = "flickr.interestingness.getList";
     const num = 40;
     const url = `${baseURL}&api_key=${key}&method=${method_interest}&per_page=${num}`;
--- src/components/sub/youtube/Youtube.jsx
+++ src/components/sub/youtube/Youtube.jsx
@@ -6,8 +6,8 @@ export default function Youtube() {
   const [Vids, setVids] = useState([]);
 
   const fetchYoute = async () => {
-    const api_key = "AIzaSyDC60bIIkAJFzy7ji4a0Eo3AX6tYudhe1w";
-    const pid = "PLYOPkdUKSFgWqafuDQN9di3uLJoTV3L3W";
+    const api_key = process.env.REACT_APP_YOUTUBE_KEY;
+    const pid = process.env.REACT_APP_PLAYLIST;
     const num = 10;
     const baseURL = `https://www.googleapis.com/youtube/v3/playlistItems?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
 
