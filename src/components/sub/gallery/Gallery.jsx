7370bc524c66819a101e306b3a291a628c9608e9
kimgyul <142677224+yul-dev-k@users.noreply.github.com>
2023-10-22 12:56:29 +0900

    mansory 컴포넌트 적용

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -1,6 +1,15 @@
 import Layout from "../../common/layout/Layout";
 import "./Gallery.scss";
+import Masonry from "react-masonry-component";
 import { useState, useEffect } from "react";
+
+/* 
+  리액트 컴포넌트에 massorny ul 적용 방법
+  1. npm i react-masonry-component
+  2. 기존의 레이아웃을 float 형태로 배치 (사이간격을 주고 싶을 때는 padding 값 사용)
+  3. 각 카드 컴포넌트를 import된 Massorny 컴포넌트로 wrapping
+  4. Massorny 컴포넌트 옵션값 설정
+*/
 
 export default function Gallery() {
   const [Pics, setPics] = useState([]);
@@ -24,25 +33,34 @@ export default function Gallery() {
   return (
     <Layout title={"Gallery"}>
       <div className="frame">
-        {Pics.map((pic, idx) => (
-          <article key={idx}>
-            <div className="pic">
-              <img
-                src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`}
-                alt={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`}
-              />
-            </div>
-            <h2>{pic.title}</h2>
+        <Masonry
+          elementType={"div"}
+          options={{ transitionDuration: "0.5" }}
+          disableImagesLoaded={false}
+          updateOnEachImageLoad={false}
+        >
+          {Pics.map((pic, idx) => (
+            <article key={idx}>
+              <div className="inner">
+                <div className="pic">
+                  <img
+                    src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`}
+                    alt={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`}
+                  />
+                </div>
+                <h2>{pic.title}</h2>
 
-            <div className="profile">
-              <img
-                src={`http://farm${pic.farm}.staticflickr.com/${pic.server}/buddyicons/${pic.owner}.jpg`}
-                alt={pic.owner}
-              />
-              <span>{pic.owner}</span>
-            </div>
-          </article>
-        ))}
+                <div className="profile">
+                  <img
+                    src={`http://farm${pic.farm}.staticflickr.com/${pic.server}/buddyicons/${pic.owner}.jpg`}
+                    alt={pic.owner}
+                  />
+                  <span>{pic.owner}</span>
+                </div>
+              </div>
+            </article>
+          ))}
+        </Masonry>
       </div>
     </Layout>
   );
