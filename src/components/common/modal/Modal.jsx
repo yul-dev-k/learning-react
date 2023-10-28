86df50bf03159d5e588eba6798be3bdaf04b70dd
kimgyul <142677224+yul-dev-k@users.noreply.github.com>
2023-10-28 15:09:15 +0900

    delay 모션 추가

--- src/components/common/modal/Modal.jsx
+++ src/components/common/modal/Modal.jsx
@@ -18,15 +18,27 @@ export default function Modal({ IsOpen, setIsOpen, children }) {
       {IsOpen && (
         <motion.aside
           className="modal"
-          initial={{ opacity: 0, x: "100%" }} // JSX가 마운트 되기 전 상태의 스타일
-          animate={{ opacity: 1, x: 0 }} // JSX가 마운트 된 후의 스타일
-          exit={{ opacity: 0, x: "-100%" }} // JSX가 앞으로 언마운트될 때의 스타일
+          initial={{ opacity: 0, x: "100%", scale: 0.5 }} // JSX가 마운트 되기 전 상태의 스타일
+          animate={{ opacity: 1, x: 0, scale: 1 }} // JSX가 마운트 된 후의 스타일
+          exit={{ opacity: 0, x: "-100%", scale: 1.5 }} // JSX가 앞으로 언마운트될 때의 스타일
           transition={{ duration: 1 }} // 스타일 변경될 때의 전환 시간
         >
-          <div className="con">
+          <motion.div
+            className="con"
+            initial={{ opacity: 0, rotate: 50 }}
+            animate={{ opacity: 1, transition: { delay: 1 }, rotate: 0 }}
+            exit={{ opacity: 0, rotate: -50 }}
+          >
             {children}
-            <span onClick={() => setIsOpen(false)}>Close</span>
-          </div>
+            <motion.span
+              onClick={() => setIsOpen(false)}
+              initial={{ opacity: 0, x: 200 }}
+              animate={{ opacity: 1, x: 0, transition: { delay: 2 } }}
+              exit={{ opacity: 0, x: 200 }}
+            >
+              Close
+            </motion.span>
+          </motion.div>
         </motion.aside>
       )}
     </AnimatePresence>
