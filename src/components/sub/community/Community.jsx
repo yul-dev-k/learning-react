725f8eee8a77aec6e3cd68b03d2b3225ed141e07
yul-dev-k <yul.dev.k@gmail.com>
2023-10-29 12:58:38 +0900

    글작성 취소버튼 아이콘연결

--- src/components/sub/community/Community.jsx
+++ src/components/sub/community/Community.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useRef } from "react";
+import { TfiWrite } from "react-icons/tfi";
+import { MdCancel } from "react-icons/md";
 import Layout from "../../common/layout/Layout";
 import "./Community.scss";
 
 export default function Community() {
+  const refInput = useRef(null);
+  const refTextarea = useRef(null);
   return (
     <Layout title={"Community"}>
-      <p>춥다</p>
+      <div className="inputBox">
+        <input type="text" ref={refInput} placeholder="title" />
+        <textarea
+          cols="30"
+          rows="3"
+          placeholder="leave messages"
+          ref={refTextarea}
+        ></textarea>
+
+        <nav>
+          <button>
+            <MdCancel color={"#555"} fontSize="20" />
+          </button>
+          <button>
+            <TfiWrite color={"#555"} fontSize="20" />
+          </button>
+        </nav>
+      </div>
     </Layout>
   );
 }
