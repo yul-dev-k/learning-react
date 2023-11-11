2fa6dfa6a0cebf2b619bdeb9a4e7842b85fc25cb
yul-dev-k <yul.dev.k@gmail.com>
2023-11-11 12:23:16 +0900

    onChange이벤트 발생시마다 실시간으로 State값 변경하고 화면 재랜더링하면서 Input요소 갱신

--- src/components/sub/members/Members.jsx
+++ src/components/sub/members/Members.jsx
@@ -4,6 +4,7 @@ import "./Members.scss";
 import { useState, useRef, useEffect } from "react";
 
 export default function Members() {
+  console.log("re-render");
   const initVal = useRef({
     userid: "",
     email: "",
@@ -15,6 +16,27 @@ export default function Members() {
     comments: "",
   });
   const [Val, setVal] = useState(initVal.current);
+
+  //value vs defultValue
+  //만약 실시간으로 바뀌는 값을 무조건 value props로 연결하고 onChange이벤트 연결
+  //바뀌지 않는 정적인 값을 연결시에는 defaultValue props로 연결하고 onChange 이벤트 연결 불필요
+
+  //onChange이벤트가 발생할때마다 해당 함수 호출
+  const handleChange = (e) => {
+    //현재 입력하고 있는 가상돔요소의 name, value값을 비구조화할당으로 뽑아냄
+    const { name, value } = e.target;
+    //객체안에서 property key값을 []로 감싸면 변수로 치환가능
+    //name='userid'인 input요소의 onChange이벤트가 발생하면
+    //[name]--> 'userid', value: 내가 현재 입력하고 있는 값 등록
+    //handleChange가 연결된 폼에 특정 값을 입력할때마다 실시간으로 해당 name값에 매칭되는 객체 property가 변경되고 변경된 값으로 State수정
+    //State가 변경될때마다 컴포넌트 재호출되면서 Input요소의 value속성으로 현재 State값이 실시간으로 출력됨
+    setVal({ ...Val, [name]: value });
+  };
+
+  useEffect(() => {
+    console.log(Val);
+  }, [Val]);
+
   return (
     <Layout title={"Members"}>
       <div className="wrap">
@@ -29,10 +51,22 @@ export default function Members() {
                 <tbody>
                   <tr>
                     <td>
-                      <input type="text" name="userid" placeholder="User ID" />
+                      <input
+                        type="text"
+                        name="userid"
+                        placeholder="User ID"
+                        value={Val.userid}
+                        onChange={handleChange}
+                      />
                     </td>
                     <td>
-                      <input type="text" name="email" placeholder="Email" />
+                      <input
+                        type="text"
+                        name="email"
+                        placeholder="Email"
+                        value={Val.email}
+                        onChange={handleChange}
+                      />
                     </td>
                   </tr>
                   <tr>
@@ -41,6 +75,8 @@ export default function Members() {
                         type="password"
                         name="pwd1"
                         placeholder="Password"
+                        value={Val.pwd1}
+                        onChange={handleChange}
                       />
                     </td>
                     <td>
@@ -48,6 +84,8 @@ export default function Members() {
                         type="password"
                         name="pwd2"
                         placeholder="Re-Password"
+                        value={Val.pwd2}
+                        onChange={handleChange}
                       />
                     </td>
                   </tr>
@@ -130,6 +168,8 @@ export default function Members() {
                         cols="30"
                         rows="5"
                         placeholder="Leave a comment"
+                        value={Val.comments}
+                        onChange={handleChange}
                       ></textarea>
                     </td>
                   </tr>
