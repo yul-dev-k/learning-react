3d10e5cb7d9f915ca79baad9cf2cb727c6e7ddbf
yul-dev-k <yul.dev.k@gmail.com>
2023-11-11 14:09:48 +0900

    마커이미지의 가로, 세로 위치값 보정

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -5,6 +5,29 @@ import "./Contact.scss";
 export default function Contact() {
   const { kakao } = window;
   const refMapContainer = useRef(null);
+  const info = useRef([
+    {
+      title: "삼성역 코엑스",
+      latlng: new kakao.maps.LatLng(37.51100661425726, 127.06162026853143),
+      imgSrc: `${process.env.PUBLIC_URL}/img/marker1.png`,
+      imgSize: new kakao.maps.Size(232, 99),
+      imgPos: { offset: new kakao.maps.Point(116, 99) },
+    },
+    {
+      title: "넥슨 본사",
+      latlng: new kakao.maps.LatLng(37.40211707077346, 127.10344953763003),
+      imgSrc: `${process.env.PUBLIC_URL}/img/marker2.png`,
+      imgSize: new kakao.maps.Size(232, 99),
+      imgPos: { offset: new kakao.maps.Point(116, 99) },
+    },
+    {
+      title: "서울 시청",
+      latlng: new kakao.maps.LatLng(37.5662952, 126.9779451),
+      imgSrc: `${process.env.PUBLIC_URL}/img/marker3.png`,
+      imgSize: new kakao.maps.Size(232, 99),
+      imgPos: { offset: new kakao.maps.Point(116, 99) },
+    },
+  ]);
 
   const mapOption = {
     center: new kakao.maps.LatLng(37.51205098440375, 127.05875950015063),
@@ -12,15 +35,10 @@ export default function Contact() {
   };
 
   // 마커 이미지 인스턴스를 생성하기 위한 정보값들
-  const imageSrc =
-    "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png";
-  const imageSize = new kakao.maps.Size(64, 69);
-  const imageOption = { offset: new kakao.maps.Point(27, 69) };
-  const markerImage = new kakao.maps.MarkerImage(
-    imageSrc,
-    imageSize,
-    imageOption
-  );
+  const imgSrc = `${process.env.PUBLIC_URL}/img/marker1.png`;
+  const imgSize = new kakao.maps.Size(232, 99);
+  const imgPos = { offset: new kakao.maps.Point(116, 99) };
+  const markerImage = new kakao.maps.MarkerImage(imgSrc, imgSize, imgPos);
   const markerPosition = mapOption.center;
   const marker = new kakao.maps.Marker({
     position: markerPosition,
