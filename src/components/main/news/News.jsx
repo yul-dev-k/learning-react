c66250e7e82e10ff4a0c7e8cff5e6d177569a22d
yul-dev-k <yul.dev.k@gmail.com>
2023-11-11 12:09:25 +0900

    사용자 컴퓨터에는 로컬저장소에 더미데이터를 초기값으로 집어넣고 출력

--- src/components/main/news/News.jsx
+++ src/components/main/news/News.jsx
@@ -1,20 +1,54 @@
 import "./News.scss";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 
-export default function News() {
+function News() {
+  const dummyData = useRef([
+    {
+      title: "title5",
+      content: "content5",
+      date: '"2023-11-05T12:35:39.686Z"',
+    },
+    {
+      title: "title4",
+      content: "content4",
+      date: '"2023-11-04T12:35:39.686Z"',
+    },
+    {
+      title: "title3",
+      content: "content3",
+      date: '"2023-11-03T12:35:39.686Z"',
+    },
+    {
+      title: "title2",
+      content: "content2",
+      date: '"2023-11-02T12:35:39.686Z"',
+    },
+    {
+      title: "title1",
+      content: "content1",
+      date: '"2023-11-01T12:35:39.686Z"',
+    },
+  ]);
   const getLocalData = () => {
     const data = localStorage.getItem("posts");
     if (data) return JSON.parse(data);
-    else return [];
+    else return dummyData.current;
   };
 
   const [News] = useState(getLocalData());
+
+  useEffect(() => {
+    localStorage.setItem("posts", JSON.stringify(News));
+  }, [News]);
+
   return (
     <section className="news">
       {News.map((post, idx) => {
-        if (idx >= 3) return null;
+        if (idx >= 5) return null;
         return <h2 key={idx}>{post.title}</h2>;
       })}
     </section>
   );
 }
+
+export default News;
