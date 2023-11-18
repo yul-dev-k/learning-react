56dae9ff845814aa7d885d1b4f1c9fe7a9bdf8e0
yul-dev-k <yul.dev.k@gmail.com>
2023-11-18 15:09:36 +0900

    스크롤시 버튼 활성화

--- src/components/main/btns/Btns.jsx
+++ src/components/main/btns/Btns.jsx
@@ -2,28 +2,44 @@ import "./Btns.scss";
 import { useRef, useEffect, useState } from "react";
 
 export default function Btns() {
-  const [Index, setIndex] = useState(0);
+  // 활성화 순번, 버튼 그룹요소, section 그룹요소가 담길 참조 객체 생성
   const num = useRef(0);
-  const handleClick = (idx) => {
-    setIndex(idx);
+  const secs = useRef(null);
+  const btns = useRef(null);
+
+  // 컴포넌트 마운티시 윈도우 스크롤 이벤트에 연결될 함수
+  const activation = () => {
+    const scroll = window.scrollY;
+
+    secs.current.forEach((el, idx) => {
+      if (scroll >= el.offsetTop - window.innerHeight / 2) {
+        Array.from(btns.current.children).forEach((btn) =>
+          btn.classList.remove("on")
+        );
+        btns.current.children[idx].classList.add("on");
+      }
+    });
   };
+
+  // 컴포넌트 마운트시
   useEffect(() => {
-    num.current = document.body.querySelectorAll(".myScroll").length;
+    // 빈 참조 객체에 버튼과 section 요소를 담아줌
+    secs.current = document.querySelectorAll(".myScroll");
+    num.current = secs.current.length;
+
+    // window scroll 이벤트에 activation함수 연결
+    window.addEventListener("scroll", activation);
+
+    return () => window.removeEventListener("scroll", activation);
   }, []);
 
   console.log(num.current);
   return (
-    <ul className="btns">
+    <ul className="btns" ref={btns}>
       {Array(num.current)
         .fill()
         .map((_, idx) => {
-          return (
-            <li
-              key={idx}
-              className={idx === Index ? "on" : ""}
-              onClick={() => handleClick(idx)}
-            ></li>
-          );
+          return <li key={idx} className={idx === 0 ? "on" : ""}></li>;
         })}
     </ul>
   );
