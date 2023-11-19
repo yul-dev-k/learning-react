32da6a23b47d6cda6d27e0f9382566575ef0e58b
yul-dev-k <yul.dev.k@gmail.com>
2023-11-19 15:16:50 +0900

    App.js 수정

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import Test from "./test";
+import Parent from "./Parent";
 import Footer from "./components/common/footer/Footer";
 import Header from "./components/common/header/Header";
-import Banner from "./components/main/banner/Banner";
-import Info from "./components/main/info/Info";
 import MainWeb from "./components/main/mainWeb/MainWeb";
-import News from "./components/main/news/News";
-import Visual from "./components/main/visual/Visual";
 import Community from "./components/sub/community/Community";
 import Contact from "./components/sub/contact/Contact";
 import Department from "./components/sub/department/Department";
@@ -20,7 +16,7 @@ import { Route, Switch } from "react-router-dom";
 function App() {
   return (
     <main className={useMedia({ tablet: 800 })}>
-      <Test />
+      <Parent />
       {/* visual은 main 라우트에만 보여야 하니 /일 때만 보여야함. 그러나 path='/'만 적혀있다면 모든 /가 붙는 경로에 다 보이게 됨. 그러므로 exact 라는 걸 포함해주면 다른 페이지에서 보이지 않음. */}
       {/* 중첩된 라우터로 복수개의 동일한 컴포넌트가 연결될 때 처음 연결라우터만 호출하고 나머지는 무시 */}
       <Switch>
