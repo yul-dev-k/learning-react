9d0c178e60be85ae3ea3434e071fecec1498ced2
yul-dev-k <yul.dev.k@gmail.com>
2023-11-25 12:33:29 +0900

    커스텀 스크롤 위치값 오류 수정

--- src/hooks/useGetCurrentScroll.js
+++ src/hooks/useGetCurrentScroll.js
@@ -5,8 +5,12 @@ export const useGetCurrentScroll = () => {
     let customScroll = 0;
     if (scroll >= refEl.current?.offsetTop - baseLine) {
       customScroll = scroll - refEl.current?.offsetTop + baseLine;
+      //스크롤 시 기준점은 커스텀 하더라도 실제 위치값이 바뀌면 안되므로 baseLine빼주는 기능을 제거
+      customScroll = scroll - refEl.current?.offsetTop;
     } else {
       customScroll = 0;
+      //마찬가지로 해당 영역을 벗어날때는 baseLine을 빼준값으로 초기화
+      customScroll = -baseLine;
     }
     return customScroll;
   };
